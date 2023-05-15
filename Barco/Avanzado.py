94c47c484ef084f6c31c6b968bc51680fca8f1a3
FranciscoAntonelli <franciscoantonelli1999@gmail.com>
2023-05-15 19:50:57 -0300

    funciones de avanzado

---
+++ Barco/Avanzado.py
@@ -0,0 +1,14 @@
+from Barco.Barco import Barco
+from Contenedor.Contenedor import Contenedor
+from Mercaderia.Mercaderia import Mercaderia
+
+class Avanzado(Barco):  
+
+    def agregar_contenedor(self, contenedor: Contenedor, mercaderia : Mercaderia):
+        
+        if (self._peso <= self._peso_max_sopor and self.cant_de_cont_cargado() <= self._cant_max_cont and 
+            self.validar_disenio(contenedor) and not self.contenedor_con_material_especial(contenedor, mercaderia)):
+            
+            self._contenedores.add(contenedor)
+        else :
+            self._peso -= contenedor.get_peso_ocupado()
