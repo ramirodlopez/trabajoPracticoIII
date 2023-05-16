1d4b6cd3c088a2a05865cf949be8d390eaf086b4
ramirodlopez <54771100+ramirodlopez@users.noreply.github.com>
2023-05-15 22:00:29 -0300

    volver agregar pedido dps de romperse

--- Pedido/Pedido.py
+++ Pedido/Pedido.py
@@ -1,13 +1,50 @@
+from Camion.Camion import Camion
+from Contenedor.Contenedor import Contenedor
+
 class Pedido:
+    def __init__(self, mercaderia, cliente, destino, servicio_completo):
+        self.mercaderia = mercaderia
+        self.cliente = cliente
+        self.destino = destino
+        self.servicio_completo = servicio_completo
+        self.precio_pedido = 0
 
-   def restoCon(self, number):
-        resto = number % 100
+    def get_mercaderia(self):
+        return self.mercaderia
 
-        cant_cien_kilos = 0
+    def set_mercaderia(self, mercaderia):
+        self.mercaderia = mercaderia
 
-        if resto == 0:
-            cant_cien_kilos = number // 100  # 300 // 100 = 3; 3*1000
-        else:
-            cant_cien_kilos = number // 100 + 1  # 469 // 100 = 4 + 1
+    def get_cliente(self):
+        return self.cliente
 
-        return cant_cien_kilos
+    def set_cliente(self, cliente):
+        self.cliente = cliente
+
+    def get_destino(self):
+        return self.destino
+
+    def set_destino(self, destino):
+        self.destino = destino
+
+    def get_servicio_completo(self):
+        return self.servicio_completo
+
+    def set_servicio_completo(self, servicio_completo):
+        self.servicio_completo = servicio_completo
+    
+    def get_precio_pedido(self):
+        return self.precio_pedido
+
+    def set_precio_pedido(self, precio_pedido):
+        self.precio_pedido = precio_pedido
+
+
+
+    
+    def usa_camion(self):
+        precio = 1
+        if(self.get_servicio_completo):
+            precio = Camion.get_precio()
+       
+        return precio
