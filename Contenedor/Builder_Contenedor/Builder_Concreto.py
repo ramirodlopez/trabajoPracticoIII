dbcbc9865c38b2434ccd54e6e53521d35b4b058f
gomezmara <maragomez195.mg@gmail.com>
2023-06-20 01:51:38 -0300

    Agrego métodos en para crear distintos tipos de contenedores en Contenedor/Director.py

--- Contenedor/Builder_Contenedor/Builder_Concreto.py
+++ Contenedor/Builder_Contenedor/Builder_Concreto.py
@@ -1,3 +1,4 @@
+from typing_extensions import override
 from Abstract_Builder import Builder
 from Contenedor.Contenedor import Contenedor
 
@@ -6,6 +7,7 @@ class BuilderConcreto(Builder):
     def __init__(self) -> None:
         pass
     
+    @override
     def reset(self, id, es_especial):
         self.contenedor = Contenedor(id, es_especial)
         return self
@@ -14,6 +16,7 @@ class BuilderConcreto(Builder):
         self.contenedor.set_largo(largo)
         return self
     
+    @override
     def asignar_ancho(self, ancho):
         self.contenedor.set_ancho(ancho)
         return self
@@ -22,26 +25,32 @@ class BuilderConcreto(Builder):
         self.contenedor.set_alto(alto)
         return self
     
+    @override
     def asignar_peso_max(self, peso_maximo):
         self.contenedor.set_peso_max(peso_maximo)
         return self
     
+    @override
     def asignar_volumen(self, volumen):
         self.contenedor.set_volumen(volumen)
         return self
 
+    @override
     def asignar_alto_exterior(self, alto_exterior):
         self.contenedor.set_alto_exterior(alto_exterior)
         return self
     
+    @override
     def asignar_largo_exterior(self, largo_exterior):
         self.contenedor.set_largo_exterior(largo_exterior)
         return self
 
+    @override
     def asignar_ancho_exterior(self, ancho_exterior):
         self.contenedor.set_ancho_exterior(ancho_exterior)
         return self
 
+    @override
     def get_contenedor(self)-> Contenedor:
         return self.contenedor
     
--- Contenedor/Director.py
+++ Contenedor/Director.py
@@ -9,6 +9,60 @@ class Director():
     def cambiar_builder(self):
         return self._builder
     
+
+    def crear_contenedor_basico(self, id, es_especial)-> Contenedor:
+        return self._builder\
+            .reset(id, es_especial)\
+            .asignar_largo(6)\
+            .asignar_ancho(2.35)\
+            .asignar_alto(2.3)\
+            .asignar_peso_max(24.000)\
+            .asignar_volumen(32.6)\
+            .asignar_alto_exterior(2.6)\
+            .asignar_largo_exterior(6.1)\
+            .asignar_ancho_exterior(2.45)\
+            .get_contenedor()
+
+    def crear_contenedor_basico_hc(self, id, es_especial)-> Contenedor:
+        return self._builder\
+            .reset(id, es_especial)\
+            .asignar_largo(12)\
+            .asignar_ancho(2.35)\
+            .asignar_alto(2.3)\
+            .asignar_peso_max(32.500)\
+            .asignar_volumen(67.7)\
+            .asignar_alto_exterior(2.6)\
+            .asignar_largo_exterior(12.1)\
+            .asignar_ancho_exterior(2.45)\
+            .get_contenedor()
+    
+
+    def crear_contenedor_flat_rack(self, id, es_especial)-> Contenedor:
+        return self._builder\
+            .reset(id, es_especial)\
+            .asignar_largo(6)\
+            .asignar_ancho(None)\
+            .asignar_alto(2.3)\
+            .asignar_peso_max(45.000)\
+            .asignar_volumen(33)\
+            .asignar_alto_exterior(2.3)\
+            .asignar_largo_exterior(6.1)\
+            .asignar_ancho_exterior(None)\
+            .get_contenedor()
+
+    def crear_contenedor_alimenticio(self, id, es_especial)-> Contenedor:
+        return self._builder\
+            .reset(id, es_especial)\
+            .asignar_largo(6)\
+            .asignar_ancho(2.35)\
+            .asignar_alto(2.3)\
+            .asignar_peso_max(24.000)\
+            .asignar_volumen(32.6)\
+            .asignar_alto_exterior(2.6)\
+            .asignar_largo_exterior(6.1)\
+            .asignar_ancho_exterior(2.45)\
+            .get_contenedor()
+    
     def crear_contenedor_open_top(self, id, es_especial)-> Contenedor:
         return self._builder\
             .reset(id, es_especial)\
@@ -18,6 +72,6 @@ class Director():
             .asignar_peso_max(45.000)\
             .asignar_volumen(67.7)\
             .asignar_alto_exterior(2.6)\
-            .asignar_largo_exterior(12.0)\
-            .asignar_ancho_exterior(2.35)\
+            .asignar_largo_exterior(12.1)\
+            .asignar_ancho_exterior(2.45)\
             .get_contenedor()
