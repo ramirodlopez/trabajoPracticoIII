b837ed28b5b605f1fb2ce3561c8fa9020464c8e5
ramirodlopez <54771100+ramirodlopez@users.noreply.github.com>
2023-06-27 19:48:10 -0300

    cambiar datos de modulo

--- modulo_contable_test/modulo_contable_test.py
+++ modulo_contable_test/modulo_contable_test.py
@@ -3,9 +3,10 @@ from unittest.mock import Mock
 from Modulo_Contable.modulo_contable import modulo_Contable
 
 class ModuloContableTests(unittest.TestCase):
-    def test_obtener_ganancia_barco_bruta_existente(self):
+    def test_obtener_ganancia_barco_bruta(self):
         empresa_mock = Mock()
         barco_mock = Mock()
+        ganancia = Mock()
         barco_mock.obtener_ganancia_barco.return_value = 1000
         
         empresa_mock.get_barcos.return_value = [barco_mock]
@@ -15,16 +16,7 @@ class ModuloContableTests(unittest.TestCase):
         
         self.assertEqual(ganancia, 1000)
        
-        
-    def test_obtener_ganancia_barco_bruta_no_existente(self):
-        empresa_mock = Mock()
-        empresa_mock.get_barcos.return_value = []
-        
-        modulo_contable = modulo_Contable(empresa_mock)
-        ganancia = modulo_contable.obtener_ganancia_barco_bruta(123)
-        
-        self.assertEqual(ganancia, 0)
-        
+  
         
     def test_calcular_gasto_combustible_barco(self):
         barco_mock = Mock()
